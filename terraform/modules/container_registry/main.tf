e66394dbb7b5d0cad1f096b0da2f109b641c1717
Socrates Chouridis <socrates.chouridis@sportion.net>
2023-05-04 11:27:37 +0300

    removed apis configuration from terraform. Changed how the access token for docker is used

--- terraform/modules/container_registry/main.tf
+++ terraform/modules/container_registry/main.tf
@@ -12,9 +12,13 @@ resource "google_artifact_registry_repository" "app_repo" {
   description   = "${var.repository_id} is managed by terraform"
   format        = "DOCKER"
 
-  docker_config {
-    immutable_tags = true
-  }
+}
+
+data "google_service_account_access_token" "access_token" {
+  provider               = google
+  target_service_account = "svc-terraform@${data.google_project.current.project_id}.iam.gserviceaccount.com"
+  scopes                 = ["cloud-platform"]
+  lifetime               = "300s"
 }
 
 # The following null_resource runs an inline script for building and pushing the docker container to the registry. It is
@@ -25,18 +29,20 @@ resource "null_resource" "image_push" {
 #    file_checksums = join(",", [for f in fileset(var.application_code, "**") : filemd5("${var.application_code}/${f}")])
     always_run = timestamp()
   }
+
   provisioner "local-exec" {
+    environment = {
+      ACCESS_TOKEN = data.google_service_account_access_token.access_token.access_token
+      IMAGE_REGISTRY = "${var.region}-docker.pkg.dev/${data.google_project.current.project_id}/${var.repository_id}"
+    }
     command = <<EOF
 #!/bin/bash
 
-IMAGE_REGISTRY=${var.region}-docker.pkg.dev/${data.google_project.current.project_id}/${var.repository_id}
-
-export ACCESS_TOKEN=$(gcloud auth print-access-token)
 echo "$ACCESS_TOKEN" | docker --config docker_config login -u oauth2accesstoken --password-stdin $IMAGE_REGISTRY
 
-docker --config docker_config build -t $IMAGE_REGISTRY/${var.repository_id}:${var.app_version} ../
+docker --config docker_config build -t $IMAGE_REGISTRY/${var.repository_id}:${var.app_version} ../ >/dev/null
 
-docker --config docker_config push $IMAGE_REGISTRY/${var.repository_id}:${var.app_version}
+docker --config docker_config push $IMAGE_REGISTRY/${var.repository_id}:${var.app_version} || exit 1
 
 rm -rf docker_config
 EOF
